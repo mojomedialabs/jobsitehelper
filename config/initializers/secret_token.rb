832be404815654fa59b02c5957baa2d3c82c11c3
Mojo Media Labs <sean@teamrgc.com>
2012-03-15 10:34:59 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobsitehelper::Application.config.secret_token = '331c2d3e71b7b1c7a8bc9b6807aa2f3ff69ae515b20e573e92094938fa88b68202693b0142e32d7983c4d7266794176213cd44060230d8882b8c8dc8b840b0ea'
